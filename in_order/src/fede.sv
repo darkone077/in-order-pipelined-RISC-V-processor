1a028d89cd363371e67478ede00922d4e93d19ad
darkone <kaushikvarmav@gmail.com>
2025-07-13 15:49:15 +0000

    added fetch-decode ff

---
+++ in_order/src/fede.sv
@@ -0,0 +1,30 @@
+module fede (
+    input clk,
+    input clr,
+    input [31:0] instrf,
+    input [31:0] pc4f,
+    input [31:0] pcf,
+    
+    output [31:0] instrd,
+    output [31:0] pc4d,
+    output [31:0] pcd
+);
+
+
+
+    always @(posedge clk) begin
+
+        if(clr) begin 
+            instrd<=0;
+            pc4d<=0;
+            pcf<=0; 
+        end
+        else begin
+            instrd<=instrf;
+            pc4d<=pc4f;
+            pcf<=pcf;
+        end
+    
+    end
+    
+endmodule
