e831fd849e06ea402853f958058416d3bbb6f7d5
darkone <kaushikvarmav@gmail.com>
2025-07-13 15:52:53 +0000

    changed posedge to negedge for clk

--- in_order/src/regfile.sv
+++ in_order/src/regfile.sv
@@ -15,7 +15,7 @@ module regfile (
 
     reg [31:0] register [0:31];
     
-    always @(posedge clk) begin
+    always @(negedge clk) begin
         if(~rst_n) begin 
             for(int i=0;i<32;i=i+1) register[i]<=32'b0;
 
