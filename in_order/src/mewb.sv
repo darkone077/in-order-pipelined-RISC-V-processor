cc97b76c574762d295fd973d581723f932c429a8
darkone <kaushikvarmav@gmail.com>
2025-07-14 15:25:22 +0000

    added memory-write back ff

---
+++ in_order/src/mewb.sv
@@ -0,0 +1,29 @@
+module mewb (
+    input clk,
+
+    //control path
+    input regWrtm,memWrtm,
+    input [1:0] rsltSrcm,
+    output regWrtw,memWrtw,
+    output [1:0] rsltSrcw,
+
+    //datapath
+    input [31:0] readDm,pc4m,
+    input [4:0] rdm,
+    output [31:0] readDw,pc4w,
+    output [4:0] rdw,
+
+);
+
+    always_ff @( clk ) begin : 
+        
+        regWrtw<=regWrtm;
+        memWrtw<=memWrtm;
+        rsltSrcw<=rsltSrcm;
+        readDw<=readDm;
+        pc4w<=pc4m;
+        rdw<=rdm;
+        
+    end
+    
+endmodule
